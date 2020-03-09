1aa9a62ded559c33fa2e960f087495ebb18718fa
arhrina <hyoukim@naver.com>
2020-03-09 16:30:38 +0900

    2020. 3. 9. pug 활용 nodejs

--- Node_07/models/bookVO.js
+++ Node_07/models/bookVO.js
@@ -1,9 +1,10 @@
 var mongoose = require("mongoose")
 var bookVO = mongoose.Schema({
-    bTitle: String,
-    bWriter: String,
-    bComp: String,
-    bPrice: Number
+
+    bTitle : String,
+    bWriter : String,
+    bComp : String,
+    bPrice : Number
 })
 
-module.exports = mongoose.model("tbl_book", bookVO)
+module.exports = mongoose.model("tbl_book",bookVO)
