5dfada4224e2846aebb48a38b309fcfa14bbe2e0
arhrina <hyoukim@naver.com>
2020-03-13 13:02:38 +0900

    2020. 3. 12. api 사용해보기. 마스크, 광주버스, 네이버

--- Node_Naver_V1/app.js
+++ Node_Naver_V1/app.js
@@ -3,28 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-var dbConn = mongoose.connection
 
-dbConn.once("open",function(){
-  console.log("MonggoDB Open OK")
-})
-dbConn.on('error',function(){
-  console.err
-})
-
-mongoose.connect("mongodb://localhost/mydb")
-
-var config = require("./config")
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
-// 위에서 생성한 config 변수(객체)를 router에게 전달하기
-var gjBusRouter = require("./routes/gjBusRouter")(app,config)
-
-
+var naverRouter = require("./routes/naverRouter")(app)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/gjbus",gjBusRouter)
+app.use("/naver",naverRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
